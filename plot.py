e73688922446a402e86d77f4193cf7741819dab3
Nachiket Patil <nacpatil@latticerobot.com>
2025-03-04 16:31:59 -0500

    added cuboids

--- plot.py
+++ plot.py
@@ -1,4 +1,4 @@
-from utils import *
+from meshClass import *
 from primitives import *
 import numpy as np
 from config import *
@@ -12,7 +12,11 @@ if __name__ == "__main__":
     # Add spheres (now stored in `mesh_objects`, not plotted immediately)
     sphere(center=(0, 0, 0), radius=1.0, title="Sphere 1")
     sphere(center=(3, 3, 3), radius=1.5, title="Sphere 2")
-    sphere(center=(-2, -2, 2), radius=10, title="Sphere 3")
+    sphere(center=(-2, -2, 2), radius=5, title="Sphere 3")
+
+    for i in range(0,5):
+        for j in range(0,5):
+            cuboid(center=(5+i*3, 8+j*3, 0), size=(2.0, 2.0, 4.0), title="3D Cuboid")
 
     # Now plot everything in a **single unified plot**
     show_all_plots()
